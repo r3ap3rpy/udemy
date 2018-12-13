5307bf18dbeab29001354c1bf3f6d080469d9411
r3ap3rpy <r3ap3rpy@gmail.com>
2018-12-13 18:03:54 +0100

    Initialized repo

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+		name = 'udemy',
+		version = '0.0.1',		
+		author = 'Szabó Dániel Ernő',
+		author_email = 'r3ap3rpy@gmail.com',
+		description = 'Python library for interacting with the udemy API 2.0!',
+		url="https://github.com/pypa/sampleproject",
+		license = 'MIT',
+		packages = ['udemy'],
+		zip_safe = False,
+		include_package_data=True,
+		install_requires=['requests'],
+      	classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+	)
